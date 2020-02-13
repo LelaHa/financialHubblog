cca77461ccf3a15aa8c341f9977fc5c18f823c1b
alan phan <alan>
2020-02-13 20:45:44 +0700

    layout

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,7 +5,7 @@ const List = styled.ul`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-  margin: 50px auto;
+  margin: 0 auto auto;
   &::after {
     content: '';
     flex: 0 0 32%;
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const Wrapper = styled.section`
-  margin: 0 auto auto;
+  margin: 50px auto;
   width: 100%;
   max-width: ${props => props.theme.sizes.maxWidth};
   padding: 3em 1.5em 2em;
